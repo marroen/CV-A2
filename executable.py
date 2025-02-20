492d43e5ae6be84311804eeb3ce90c95c13588b3
Martin Ronning <ronningmartin@outlook.com>
2025-02-20 22:09:57 +0100

    remove return

--- executable.py
+++ executable.py
@@ -80,8 +80,6 @@ def main():
     
     #get_intrinsics()
 
-    return
-
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
