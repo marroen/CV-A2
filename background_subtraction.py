87e6dc3cff8080e99ad21953c6721f9a101ac9c4
3missary <rwbohm@shockers.wichita.edu>
2025-02-24 14:23:24 +0100

    cleaned up code for foreground frame generation, added voxel reconstruction file for task 3

--- background_subtraction.py
+++ background_subtraction.py
@@ -11,46 +11,45 @@ width = 644
 height = 486
 resolution = (width, height)
 
-# Create separate Gaussian models for each camera
-# Stores the mean and variance of HSV values for each pixel in the background
-# Eg. for pixel (y,x): [y, x, [mean_H, mean_S, mean_V, var_H, var_S, var_V]]
-cam1_background_model = np.zeros((height, width, 6), dtype=np.float32)
-cam2_background_model = np.zeros((height, width, 6), dtype=np.float32)
-cam3_background_model = np.zeros((height, width, 6), dtype=np.float32)
-cam4_background_model = np.zeros((height, width, 6), dtype=np.float32)
-
-# For each background model, initialize variance for H, S, V to small value (to avoid dividing by 0 later)
-for model in [cam1_background_model, cam2_background_model, cam3_background_model, cam4_background_model]:
-    model[:, :, 3:] = 1e-6
-
-# Computes the gaussian distribution of the given video and stores this in the given background model
-def computeBackgroundModel(background_model, vid, resolution):
+# Computes the gaussian distribution given EITHER a video OR an hsv_frames array and stores this in the given background model
+def compute_gaussian_model(resolution, vid=None, hsv_frames=None):
 
     width, height = resolution
-    hsv_frames = []
 
-    # Iterate through each frame
+    # Stores the mean and variance of HSV values for each pixel in the frame
+    # Eg. for pixel (y,x): [y, x, [mean_H, mean_S, mean_V, var_H, var_S, var_V]]
+    gaussian_model = np.zeros((height, width, 6), dtype=np.float32)
+
+    # Initialize variance for H, S, V to small value (to avoid dividing by 0 later)
+    gaussian_model[:, :, 3:] = 1e-6
+
+    # If passed a video, process video
     frame_count = 0
-    while True:
+    if hsv_frames is None:
+        hsv_frames = []
 
-        success, frame = vid.read()  # Reads the next frame
-        if not success:
-            cv.imshow("Frame", last_frame)
-            key = cv.waitKey(0)  # Waits for keypress
-            if key == 27:  # Halt if ESC pressed
-                exit()
-            break  # Break when video ends
+        # Iterate through each frame
+        while True:
 
-        # Convert frame to HSV
-        hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            success, frame = vid.read()  # Reads the next frame
+            if not success:
+                cv.imshow("Frame", last_frame)
+                key = cv.waitKey(0)  # Waits for keypress
+                if key == 27:  # Halt if ESC pressed
+                    exit()
+                break  # Break when video ends
 
-        # Append frame to array
-        hsv_frames.append(hsv_frame.copy())
+            # Convert frame to HSV
+            hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-        print(f"Processing frame {frame_count}")
+            # Append frame to array
+            hsv_frames.append(hsv_frame.copy())
 
-        frame_count += 1
-        last_frame = frame
+            print(f"Processing frame {frame_count}")
+
+            frame_count += 1
+            last_frame = frame
+            vid.release()
 
     # Convert frames to numpy array
     # Eg. [num_frames, height, width, [H, S, V]]
@@ -58,14 +57,15 @@ def computeBackgroundModel(background_model, vid, resolution):
 
     # Iterates through each HSV value of each pixel of each frame, and calculates the mean and variance
     # TODO: find other algs for variation https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-    cam1_background_model[:, :, :3] = np.mean(hsv_frames, axis=0)
-    cam1_background_model[:, :, 3:] = np.var(hsv_frames, axis=0)
+    gaussian_model[:, :, :3] = np.mean(hsv_frames, axis=0)
+    gaussian_model[:, :, 3:] = np.var(hsv_frames, axis=0)
 
-    vid.release()
+
     print(f"\nProcessed {frame_count} frames")
+    return gaussian_model
 
 # Performes background subtraction on the given video compared to the given background model
-def backgroundSubtraction(background_model, vid, resolution):
+def background_subtraction(background_model, vid, resolution):
 
     width, height = resolution
     hsv_frames = []
@@ -113,12 +113,78 @@ def backgroundSubtraction(background_model, vid, resolution):
     vid.release()
     print(f"\nProcessed {frame_count} frames")
 
+# IN PROGRESS
+# IDEA: get the gaussian distribution of the 5 frame buffer and find distance to background
+# Didn't work very well
+def background_subtraction_buffer(background_model, vid, resolution):
+
+    width, height = resolution
+    hsv_frames = []
+    buffer_size = 5
+
+    # Iterate through each frame
+    frame_count = 0
+    while True:
+
+        success, frame = vid.read()  # Reads the next frame
+        if not success:
+            break  # Break when video ends
+
+        # Convert frame to HSV
+        hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        # Keep only the last 5 frames in buffer
+        if len(hsv_frames) > 5:
+            hsv_frames.pop(0)
+
+        # Wait until we have at least 5 frames
+        if len(hsv_frames) < 5:
+            print(f"Skipping frame {frame_count} (not enough frames yet)")
+            hsv_frames.append(hsv_frame.copy())
+            frame_count += 1
+            continue
+
+        threshold = 20  # Number of standard deviations
+
+        foreground_model = compute_gaussian_model(resolution, hsv_frames=np.array(hsv_frames))
+
+        # Difference of frame HSV and background mean HSV
+        # Eg. (foreground mean HSV [05, 20, 50]) - (background mean HSV [50, 50, 05]) = [45, 30, 0]
+        mean_diff = foreground_model[:, :, :3] - background_model[:, :, :3]
+
+        # Finds the distance between the foreground frame value and the background model distribution
+        mahalanobis_dist = (
+            (mean_diff[:, :, 0] ** 2) / (background_model[:, :, 0] + 1e-6) + # H
+            (mean_diff[:, :, 1] ** 2) / (background_model[:, :, 1] + 1e-6) + # S
+            (mean_diff[:, :, 2] ** 2) / (background_model[:, :, 2] + 1e-6)   # V
+        )
+
+        # Initializes foreground frame with all 0's
+        foreground_frame = np.zeros(mahalanobis_dist.shape, dtype=np.uint8)
+        foreground_frame[mahalanobis_dist > threshold] = 255 # Set any pixel outide threshold to white
+
+        cv.imshow("Foreground", foreground_frame)
+        key = cv.waitKey(0)  # Waits for keypress
+        if key == 27:  # Halt if ESC pressed
+            exit()
+
+        # Append frame to array
+        hsv_frames.append(hsv_frame.copy())
+
+        print(f"Processing frame {frame_count}")
+
+        frame_count += 1
+
+    vid.release()
+    print(f"\nProcessed {frame_count} frames")
+
 
 # Compute the background model for cam1
-computeBackgroundModel(cam1_background_model, background_vid, resolution)
+cam1_background_model = compute_gaussian_model(resolution, vid=background_vid)
 
 # Use the cam1 background model for background subtraction on the foreground video
-backgroundSubtraction(cam1_background_model, foreground_vid, resolution)
+background_subtraction(cam1_background_model, foreground_vid, resolution)
+#background_subtraction_buffer(cam1_background_model, foreground_vid, resolution)
 
 
 # TEST to see if the background model worked
